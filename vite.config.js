93d1be4de9dad4c28c44e9748f742f99901b3d0d
Serdar Savaş <srdrsvs56@gmail.com>
2026-07-11 12:11:16 +0300

    Fix React Router refresh on Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- EKLENTİYİ İÇERİ ALDIK
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <-- TAILWIND V4'Ü BURADA AKTİF ETTİK
-  ],
-  base: './', // Canlı sunucuda yolların kaybolmasını önler
-})
+  plugins: [react(), tailwindcss()],
+  base: "/", // <-- BUNU DEĞİŞTİR
+});
